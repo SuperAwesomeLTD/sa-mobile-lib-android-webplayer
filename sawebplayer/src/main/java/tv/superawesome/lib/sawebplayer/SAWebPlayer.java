8371fce038f3e59c2502857b513c4e0f58904e46
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-01-30 12:23:58 +0000

    0.3.1 - Removed the SAWebView class because it wasn't needed and moved all that functionality into a normal Web View

--- sawebplayer/src/main/java/tv/superawesome/lib/sawebplayer/SAWebPlayer.java
+++ sawebplayer/src/main/java/tv/superawesome/lib/sawebplayer/SAWebPlayer.java
@@ -1,6 +1,7 @@
 package tv.superawesome.lib.sawebplayer;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -8,35 +9,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
-public class SAWebPlayer extends Fragment implements SAWebPlayerEventInterface {
+public class SAWebPlayer extends Fragment {
 
+    // boolean holding whether the web view has finished loading or not
+    private boolean finishedLoading = false;
+
+    // private variables for the web player
     private FrameLayout holder = null;
-    private SAWebView webView = null;
-    private int contentWidth = 0;
-    private int contentHeight = 0;
+    private WebView     webView = null;
+    private int         contentWidth = 0;
+    private int         contentHeight = 0;
 
     // interface objects used for the web player callback mechanism
     private SAWebPlayerEventInterface eventListener;
 
+    /**
+     * Constructor
+     */
     public SAWebPlayer() {
         //setup the default event listener, so as it's never null
         eventListener = new SAWebPlayerEventInterface() {@Override public void saWebPlayerDidReceiveEvent(SAWebPlayerEvent event, String destination) {}};
     }
 
-    public SAWebPlayer(int width, int height) {
-        this();
+    /**
+     * Main method that sets the desired content size to be displayed
+     *
+     * @param width  width of the content size
+     * @param height height of the content size
+     */
+    public void setContentSize (int width, int height) {
         contentWidth = width;
         contentHeight = height;
     }
 
+    /**
+     * Overridden fragment "onCreate" method. This just specifies the fragment to be retain.
+     * This will be called only once.
+     *
+     * @param savedInstanceState a saved instance state; can be null.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
     }
 
+    /**
+     * Overridden fragment "onCreateView" method in which all the subviews needed by the fragment
+     * are created or laid out. This will be called every time the configuration of the app
+     * changes (e.g. on orientation).
+     *
+     * @param inflater           current inflater
+     * @param container          view that contains the fragment
+     * @param savedInstanceState previous saved state
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 
@@ -46,10 +77,36 @@ public class SAWebPlayer extends Fragment implements SAWebPlayerEventInterface {
             holder.setClipChildren(false);
             holder.setClipToPadding(false);
 
-            webView = new SAWebView(getActivity());
+            webView = new WebView(getActivity());
             webView.setBackgroundColor(Color.TRANSPARENT);
-            webView.eventListener = this;
-            saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Prepared, null);
+            webView.setInitialScale(100);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                    if (shouldOverrideUrlLoading(view, url)) {
+                        view.stopLoading();
+                    }
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    finishedLoading = true;
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    if (finishedLoading) {
+                        eventListener.saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Click, url);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
+            eventListener.saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Prepared, null);
         }
         else {
             holder.removeView(webView);
@@ -76,13 +133,24 @@ public class SAWebPlayer extends Fragment implements SAWebPlayerEventInterface {
                 webView.setTranslationX(sizes.left);
                 webView.setTranslationY(sizes.top);
 
-                saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Layout, null);
+                // send event
+                eventListener.saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Layout, null);
+
             }
         });
 
         return holder;
     }
 
+    /**
+     * Private method that maps a source rect into a bounding rect.
+     *
+     * @param sourceW   source width
+     * @param sourceH   source height
+     * @param boundingW bounding width
+     * @param boundingH bounding height
+     * @return          the resulting correct rect
+     */
     private Rect mapSourceSizeIntoBoundingSize(float sourceW, float sourceH, float boundingW, float boundingH) {
         float sourceRatio = sourceW / sourceH;
         float boundingRatio = boundingW / boundingH;
@@ -102,24 +170,58 @@ public class SAWebPlayer extends Fragment implements SAWebPlayerEventInterface {
         return new Rect((int)X, (int)Y, (int)W, (int)H);
     }
 
+    /**
+     * Method that loads the HTML into the underlining web view
+     *
+     * @param html the html to load
+     */
     public void loadHTML (String html) {
         if (webView != null) {
-            webView.loadHTML(html);
+
+            // if the HTML is null, just return by default and don't do anything
+            if (html == null) return;
+
+            // replace HTML template parameters with their corresponding actual values,
+            // before loading the final HTML into the web player
+            String baseHtml = "<html><header><style>html, body, div { margin: 0px; padding: 0px; width: 100%; height: 100%; }</style></header><body>_CONTENT_</body></html>";
+            String fullHtml = baseHtml.replace("_CONTENT_", html);
+
+            // load data directly, not from file as before
+            webView.loadData(fullHtml, "text/html", "UTF-8");
+
+            // call success listener
+            eventListener.saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Loaded, null);
+
+            // webView.loadHTML(html);
         }
     }
 
+    /**
+     * Getter method for the view holder (frame layout) that acts as first child to the web player
+     *
+     * @return holder instance
+     */
     public FrameLayout getHolder () {
         return holder;
     }
 
-    public SAWebView getWebView () {
+    /**
+     * Getter method for the web view
+     *
+     * @return web view instance
+     */
+    public WebView getWebView () {
         return webView;
     }
 
+    /**
+     * Overridden fragment "onStart" method that sends out an event
+     *
+     */
     @Override
     public void onStart() {
         super.onStart();
-        saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Started, null);
+        eventListener.saWebPlayerDidReceiveEvent(SAWebPlayerEvent.Web_Started, null);
     }
 
     /**
@@ -129,10 +231,5 @@ public class SAWebPlayer extends Fragment implements SAWebPlayerEventInterface {
      */
     public void setEventListener(SAWebPlayerEventInterface listener) {
         eventListener = listener != null ? listener : eventListener;
-    }
-
-    @Override
-    public void saWebPlayerDidReceiveEvent(SAWebPlayerEvent event, String destination) {
-        eventListener.saWebPlayerDidReceiveEvent(event, destination);
     }
 }
